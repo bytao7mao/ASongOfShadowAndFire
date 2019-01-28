136bbd21ad6495dda04779548687b79c814c0072
Marius Nicolae <Marius.A.Nicolae@outlook.com>
2019-01-28 11:53:58 +0200

    found some nasty code :) and had to clean it

--- app/src/main/java/com/example/tao/myapplication/GameMenuActivity.java
+++ app/src/main/java/com/example/tao/myapplication/GameMenuActivity.java
@@ -4,7 +4,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.media.MediaPlayer;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
@@ -40,66 +39,20 @@ public class GameMenuActivity
     Button optionsBtn;
     MediaPlayer menuMusic;
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_menu_activity);
         ButterKnife.bind(this);
-
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
-
         menuMusic = MediaPlayer.create(GameMenuActivity.this, R.raw.medici);
         if (getApplicationContext() != null) {
             Typeface typefaceBoldItalic = Typeface.createFromAsset
                     (getAssets(), "fonts/ACaslonPro-BoldItalic.otf");
             tvStartGame.setTypeface(typefaceBoldItalic);
         }
-
         menuMusic.start();
         menuMusic.setLooping(true);
-        //start new game sending to NewGameActivity intent
-        startGame.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                configureStartBtn();
-            }
-
-            private void configureStartBtn() {
-                menuMusic.stop();
-                userName = editTxt.getText().toString();
-                if (TextUtils.isEmpty(userName)) {
-                    editTxt.setError("Please input a userName");
-                    return;
-                }
-                Intent i = new Intent(GameMenuActivity.this, NewGameActivity.class);
-                i.putExtra(NAME, userName);
-                startActivity(i);
-            }
-        });
-
-
-        optionsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
-                    boolean settingsCanWrite = Settings.System.canWrite(getApplicationContext());
-                    if(!settingsCanWrite) {
-                        // If do not have write settings permission then open the Can modify system settings panel.
-                        Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
-                        startActivity(intent);
-                    }else {
-                        // If has permission then show an alert dialog with message.
-                        AlertDialog alertDialog = new AlertDialog.Builder(GameMenuActivity.this).create();
-                        alertDialog.setMessage("You have system write settings permission now.");
-                        alertDialog.show();
-                    }
-                }
-                startActivity(new Intent(GameMenuActivity.this, OptionsActivity.class));
-            }
-        });
-
-
     }
 
 
@@ -112,16 +65,46 @@ public class GameMenuActivity
             case R.id.newGamebtn:
                 btnName = "New Game Clicked";
                 Log.d(GameMenuActivity.class.getSimpleName() + "", btnName);
+                //start new game sending to NewGameActivity intent
+                configureStartNewButton();
                 break;
             case R.id.optionsBtn:
                 btnName = "Options Btn clicked";
                 Log.d(GameMenuActivity.class.getSimpleName() + "", btnName);
-                startActivity(new Intent(GameMenuActivity.this, OptionsActivity.class));
+//                startActivity(new Intent(GameMenuActivity.this, OptionsActivity.class));
+                optionsBtnStart();
                 break;
             default:
                 btnName = "OtherButton";
                 Log.d(GameMenuActivity.class.getSimpleName() + "", btnName);
                 break;
         }
+    }
+    private void configureStartNewButton() {
+        menuMusic.stop();
+        userName = editTxt.getText().toString();
+        if (TextUtils.isEmpty(userName)) {
+            editTxt.setError("Please input a userName");
+            return;
+        }
+        Intent i = new Intent(GameMenuActivity.this, NewGameActivity.class);
+        i.putExtra(NAME, userName);
+        startActivity(i);
+    }
+    private void optionsBtnStart(){
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            boolean settingsCanWrite = Settings.System.canWrite(getApplicationContext());
+            if(!settingsCanWrite) {
+                // If do not have write settings permission then open the Can modify system settings panel.
+                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                startActivity(intent);
+            }else {
+                // If has permission then show an alert dialog with message.
+                AlertDialog alertDialog = new AlertDialog.Builder(GameMenuActivity.this).create();
+                alertDialog.setMessage("You have system write settings permission now.");
+                alertDialog.show();
+            }
+        }
+        startActivity(new Intent(GameMenuActivity.this, OptionsActivity.class));
     }
 }
